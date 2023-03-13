4b871829686215ec01f8bc94ec0375fda2c41da9
BankTNBD <bankthanabadee2007@icloud.com>
2023-03-13 23:24:07 +0700

    add unfinish bingo

--- Assets/Scripts/MiniGame/Bingo/RandomNum.cs
+++ Assets/Scripts/MiniGame/Bingo/RandomNum.cs
@@ -11,7 +11,7 @@ public class RandomNum : MonoBehaviour
     private int showIndex = 0;
     public int[] randomNum;
     public int[] rightNum;
- 
+    private int[] bingoBoard = new int[25] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
     void Start()
     {
         System.Random random = new System.Random();
@@ -54,7 +54,62 @@ public class RandomNum : MonoBehaviour
 
     public void randomRightNumber()
     {
+        bool correctNum = false;
+        int indexN = 0;
         rightNumShow.text = "" + rightNum[showIndex];
+        for (int i = 0; i < bingoTable.Length; i++)
+        {
+            if (bingoTable[i].text == "" + rightNum[showIndex])
+            {
+                indexN = i;
+                correctNum = true;
+                break;
+            }
+        }
+        if (correctNum)
+        {
+            bingoTable[indexN].color = new Color(1f, 0f, 0f);
+            bingoBoard[indexN] = -1;
+        }
+        if (IsBingoPattern())
+        {
+            Debug.Log("Bingo");
+        }
         showIndex++;
+    }
+
+    bool IsBingoPattern()
+    {
+        // Check for horizontal Bingo pattern
+        for (int i = 0; i <= 20; i += 5)
+        {
+            if (bingoBoard[i] == -1 && bingoBoard[i + 1] == -1 && bingoBoard[i + 2] == -1 && bingoBoard[i + 3] == -1 && bingoBoard[i + 4] == -1)
+            {
+                return true;
+            }
+        }
+
+        // Check for vertical Bingo pattern
+        for (int i = 0; i <= 4; i++)
+        {
+            if (bingoBoard[i] == -1 && bingoBoard[i + 5] == -1 && bingoBoard[i + 10] == -1 && bingoBoard[i + 15] == -1 && bingoBoard[i + 20] == -1)
+            {
+                return true;
+            }
+        }
+
+        // Check for diagonal Bingo pattern (top-left to bottom-right)
+        if (bingoBoard[0] == -1 && bingoBoard[6] == -1 && bingoBoard[12] == -1 && bingoBoard[18] == -1 && bingoBoard[24] == -1)
+        {
+            return true;
+        }
+
+        // Check for diagonal Bingo pattern (top-right to bottom-left)
+        if (bingoBoard[4] == -1 && bingoBoard[8] == -1 && bingoBoard[12] == -1 && bingoBoard[16] == -1 && bingoBoard[20] == -1)
+        {
+            return true;
+        }
+
+        return false; // No Bingo pattern was found
     }
 }
---
+++ Assets/Scripts/MiniGame/TakPla/FishMove.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishMove : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    float range;
+    [SerializeField]
+    float maxDistance;
+
+    Vector2 wayPoint;
+
+    void Start()
+    {
+        SetNewDestination();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, wayPoint, speed * Time.deltaTime);
+        if (Vector2.Distance(transform.position, wayPoint) < range)
+        {
+            SetNewDestination();
+        }
+    }
+
+    void SetNewDestination()
+    {
+        wayPoint = new Vector2(Random.Range(-maxDistance, maxDistance), Random.Range(-maxDistance, maxDistance));
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("HIT");
+        SetNewDestination();
+    }
+}
